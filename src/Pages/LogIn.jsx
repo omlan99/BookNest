e9828def4b1be09d6e48ed3a3c2c0a9e708c1c55
omlan99 <omlan10616@gmail.com>
2024-12-22 15:09:14 +0600

    added handleLogin function

--- src/Pages/LogIn.jsx
+++ src/Pages/LogIn.jsx
@@ -1,17 +1,25 @@
 import { Link } from "react-router-dom";
 
 const LogIn = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+  };
   return (
     <div className="hero bg-base-200 min-h-screen ">
       <div className="hero-content flex-col lg:flex-row-reverse ">
         <div className="card bg-base-100 w-full max-w-xl  shadow-2xl">
           <h1 className="text-3xl font-bold">Login now!</h1>
-          <form className="w-96 m-4">
+          <form className="w-96 m-4" onSubmit={handleLogin}>
             <div className="form-control">
               <label className="label ">
                 <span className="label-text ">Email</span>
               </label>
               <input
+                name="email"
                 type="email"
                 placeholder="email"
                 className="input input-bordered"
@@ -23,6 +31,7 @@ const LogIn = () => {
                 <span className="label-text">Password</span>
               </label>
               <input
+                name="password"
                 type="password"
                 placeholder="password"
                 className="input input-bordered"
@@ -40,7 +49,13 @@ const LogIn = () => {
           </form>
           <div className="divider"></div>
           <div>
-             <p>Don't have an account? Please <Link to={'/signUp'} className="text-purple-500"> Sign Up</Link></p>
+            <p>
+              Don't have an account? Please{" "}
+              <Link to={"/signUp"} className="text-purple-500">
+                {" "}
+                Sign Up
+              </Link>
+            </p>
           </div>
         </div>
       </div>
