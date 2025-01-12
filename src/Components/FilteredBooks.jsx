6b896ade5c3d306ce0350fb56e3f39a173da0fac
omlan99 <omlan10616@gmail.com>
2025-01-13 00:33:40 +0600

    fetched data by category

--- src/Components/FilteredBooks.jsx
+++ src/Components/FilteredBooks.jsx
@@ -1,28 +1,16 @@
+import axios from 'axios';
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 const FilteredBooks = () => {
     const {category} = useParams()
     console.log(category)
-    // useEffect(() =>{
-    //     fetch("http://localhost:5000/category?category=Fiction",{
-          
-    //             method : "GET",
-    //             headers : {
-                    
-    //                 "content-type" : 'application/json',
-        
-    //             },
-    //             // body : JSON.stringify()
-                    
-
-    //     })
-    //     .then(res => res.json())
-    //     .then(data => {
-    //         console.log(data)
-
-    //     })
-    // },[])
+    useEffect(() =>{
+        axios.get(`http://localhost:5000/category?category=${category}`)
+        .then(res => {
+            console.log(res.data)
+        })
+    },[])
     return (
         <div>
             <h2>Filtered product component {category}</h2>
