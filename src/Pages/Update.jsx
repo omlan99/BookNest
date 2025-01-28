c3855eb82a8f470741c3f4be21cc6db7d707890c
omlan99 <omlan10616@gmail.com>
2025-01-28 21:19:49 +0600

    removed unneccesaty comments

--- src/Pages/Update.jsx
+++ src/Pages/Update.jsx
@@ -5,143 +5,44 @@ import { IoChevronDownCircleOutline } from "react-icons/io5";
 import { useParams } from "react-router-dom";
 
 const Update = () => {
-    const [categories, setCategories] = useState([]);
-    const {id} = useParams()
-
+  const [categories, setCategories] = useState([]);
+  const { id } = useParams();
 
   useEffect(() => {
     // axios.get('http://localhost:5000')
-    axios.get('https://book-nest-server-wine.vercel.app/')
-      .then((res) => {
-        // console.log(res.data);
-       const  getData = res.data
-        const uniqueCategories = [
-          ...new Set(getData.map((book) => book.category)),
-        ];
-        setCategories(uniqueCategories);
-        // console.log(uniqueCategories);
-      });
+    axios.get("https://book-nest-server-wine.vercel.app/").then((res) => {
+      // console.log(res.data);
+      const getData = res.data;
+      const uniqueCategories = [
+        ...new Set(getData.map((book) => book.category)),
+      ];
+      setCategories(uniqueCategories);
+      // console.log(uniqueCategories);
+    });
   }, []);
 
-  const {register, handleSubmit, formState: { errors }} = useForm()
-  const onSubmit = (data) =>{
-    console.log(data)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
     // axios.patch(`http://localhost:5000/update/${id}`, data)
-    axios.patch(`https://book-nest-server-wine.vercel.app/update/${id}`, data)
-    .then(res => console.log(res.data))
-  } 
+    axios
+      .patch(`https://book-nest-server-wine.vercel.app/update/${id}`, data)
+      .then((res) => console.log(res.data));
+  };
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="space-y-12">
-        
-
           <div className="border-b border-gray-900/10 py-12">
             <h2 className="text-4xl font-bold text-gray-900 ">
               Update Book Information
             </h2>
 
-
-
             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-              {/* <div className="sm:col-span-3">
-                <label
-                  htmlFor="first-name"
-                  className="block text-sm/6 font-medium text-gray-900"
-                >
-                  First name
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="first-name"
-                    name="first-name"
-                    type="text"
-                    autoComplete="given-name"
-                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                  />
-                </div>
-              </div>
-
-              <div className="sm:col-span-3">
-                <label
-                  htmlFor="last-name"
-                  className="block text-sm/6 font-medium text-gray-900"
-                >
-                  Last name
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="last-name"
-                    name="last-name"
-                    type="text"
-                    autoComplete="family-name"
-                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                  />
-                </div>
-              </div>
-
-              <div className="sm:col-span-4">
-                <label
-                  htmlFor="email"
-                  className="block text-sm/6 font-medium text-gray-900"
-                >
-                  Email address
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                  />
-                </div>
-              </div>
-
-              <div className="sm:col-span-3">
-                <label
-                  htmlFor="country"
-                  className="block text-sm/6 font-medium text-gray-900"
-                >
-                  Country
-                </label>
-                <div className="mt-2 grid grid-cols-1">
-                  <select
-                    id="country"
-                    name="country"
-                    autoComplete="country-name"
-                    className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pl-3 pr-8 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                  >
-                    <option>United States</option>
-                    <option>Canada</option>
-                    <option>Mexico</option>
-                  </select>
-                  <IoChevronDownCircleOutline
-                    aria-hidden="true"
-                    className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
-                  />
-                </div>
-              </div>
-
-              <div className="col-span-full">
-                <label
-                  htmlFor="street-address"
-                  className="block text-sm/6 font-medium text-gray-900"
-                >
-                  Street address
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="street-address"
-                    name="street-address"
-                    type="text"
-                    autoComplete="street-address"
-                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                  />
-                </div>
-              </div> */}
-
-
               <div className="sm:col-span-2 sm:col-start-1">
                 <label
                   htmlFor="name"
@@ -154,7 +55,6 @@ const Update = () => {
                     id="name"
                     {...register("bookName")}
                     type="text"
-                 
                     className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                   />
                 </div>
@@ -165,14 +65,13 @@ const Update = () => {
                   htmlFor="image"
                   className="block text-sm/6 font-medium text-gray-900"
                 >
-                 Book Image
+                  Book Image
                 </label>
                 <div className="mt-2">
                   <input
                     id="image"
                     {...register("image")}
                     type="url"
-
                     className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                   />
                 </div>
@@ -190,14 +89,10 @@ const Update = () => {
                     {...register("author")}
                     name="author"
                     type="text"
-               
                     className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                   />
                 </div>
               </div>
-
-
-         
 
               <div className="sm:col-span-3">
                 <label
@@ -212,9 +107,9 @@ const Update = () => {
                     name="country"
                     className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pl-3 pr-8 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                   >
-                    {
-                        categories.map(category =><option>{category}</option> )
-                    }
+                    {categories.map((category) => (
+                      <option>{category}</option>
+                    ))}
                   </select>
                   <IoChevronDownCircleOutline
                     aria-hidden="true"
@@ -234,19 +129,16 @@ const Update = () => {
                     id="rating"
                     name="author"
                     type="text"
-               
                     className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                   />
                 </div>
               </div>
             </div>
           </div>
-
-         
         </div>
-        <button className="btn btn-wide my-4 bg-[#018567] text-white">Submit</button>
-
-                    
+        <button className="btn btn-wide my-4 bg-[#018567] text-white">
+          Submit
+        </button>
       </form>
     </div>
   );
